4204768078961efca7d917f9db81673c723f692d
Erina <erina.moore@student.swic.edu>
2024-02-24 23:17:36 -0600

    Rewrite the Greet function to use Arrow function

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-function addGreet(name) {
-return `Hello ${name}!`;
-}
+const addGreet = (name) => `Hello ${name}!`;
 
 const greeting = addGreet("Erina");
- 
+
 console.log(`Hello, my name is ${greeting} 
 I have a dog named Moose`);
+
