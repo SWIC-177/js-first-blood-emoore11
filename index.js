ebede289646c15240fe1879f5f84db511936f8a4
Erina <erina.moore@student.swic.edu>
2024-04-09 14:22:08 -0500

    Fix linting errors

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const person = {
-  name: "John Doe",
-  age: 23,
-};
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "Jim Doe",
+    age: 34,
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+    age: 45,
+  },
+  {
+    id: 5,
+    name: "Jack Doe",
+    age: 56,
+  },
+  {
+    id: 6,
+    name: "Jenny Doe",
+    age: 67,
+  },
+];
 
-const updatePersonName = (p, newName) => ({
-  ...p,
-  name: newName,
-  hobbies: ["reading", "writing"],
-});
+function renameOdds(p) {
+  const newName = p.map((odd) => ({ ...odd }));
 
-const updatedPerson = updatePersonName(person, "Jane Doe");
-console.log(updatedPerson);
+  for (let i = 0; i < newName.length; i += 1) {
+    if (i % 2 !== 0) {
+      newName[i].name = "Odd Name";
+    }
+  }
+
+  return newName;
+}
+
+console.log(renameOdds(people));
