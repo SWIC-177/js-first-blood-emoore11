aaf1607c38e2fa8dcd0060fb0c3fc8ca16997c8c
Erina <erina.moore@student.swic.edu>
2024-04-09 13:38:18 -0500

    Shorten function using Arrow Function

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 const person = {
   name: "John Doe",
   age: 23,
-  address: {
-    street: "123 Main St",
-    city: "New York",
-    state: "NY",
-    zip: "10001",
-  },
 };
-function updatePersonName(p, newName) {
-  const p2Update = { ...p };
 
-  p2Update.name = newName;
-  return p;
-}
+const updatePersonName = (p, newName) => ({
+  ...p,
+  name: newName,
+  hobbies: ["reading", "writing"],
+});
 
 const updatedPerson = updatePersonName(person, "Jane Doe");
 console.log(updatedPerson);
